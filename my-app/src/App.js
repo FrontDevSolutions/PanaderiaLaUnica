fb350ea81974d4000deee2842429bbabae6354f9
Alejandra Gómez <AlejaGomezSanchez@Outlook.com>
2022-10-26 23:40:43 -0500

    Carrito

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Carrito from "./components/Carrito";
+
+function App(){
+  return(
+    <div>
+    <h1>useReducer</h1>
+    <hr/>
+    <Carrito/> 
+    </div>
+  );
+}
+
+export default App;
