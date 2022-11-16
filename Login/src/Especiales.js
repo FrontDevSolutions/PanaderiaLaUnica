21fc3587ec7fc33cc70c952b94e8d5e3b76f584c
Alejandra Gómez <AlejaGomezSanchez@Outlook.com>
2022-11-16 10:53:11 -0500

    Feature/ Login

---
+++ Login/src/Especiales.js
@@ -0,0 +1,62 @@
+import './Especiales.css';
+
+function Especiales() {
+
+    return (
+        <header>
+        <nav>
+          <div className="navegacion">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+            width="48" height="48"
+            viewBox="0 0 48 48"
+            style=" fill:#000000;"><path fill="#eb6773" d="M37,6c0-1.105-0.895-2-2-2H13c-1.105,0-2,0.895-2,2v4h26V6z"></path><path fill="#b31523" d="M11,41.72c0,0.996,1.092,1.606,1.94,1.084L24,35.998l11.06,6.806C35.908,43.326,37,42.716,37,41.72	V30H11V41.72z"></path><rect width="26" height="12" x="11" y="18" fill="#cf1928"></rect><rect width="26" height="8" x="11" y="10" fill="#d9414f"></rect> 
+            </svg>
+            <a href="">Inicio</a>
+            <a href="">Nosotros</a>
+            <a href="">Tienda</a>
+          </div>
+      </nav>
+
+    <div className="imagen imagen__titulo">
+      <h4 >Hola a todo</h4>
+    </div>
+
+<section className="contenedor">
+    <div className="contenedor__centrar">
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="96" height="96"
+          viewBox="0 0 48 48"
+          style=" fill:#000000;"><path fill="#0883d9" d="M36.232,23.985c0-5.865,4.766-8.51,4.966-8.636c-2.596-3.993-6.79-4.467-8.362-4.468	c-3.643,0-6.863,2.022-8.585,2.022c-1.797,0-4.418-2.121-7.363-2.022c-3.843,0.075-7.363,2.346-9.334,5.691	c-1.397,2.396-1.947,5.217-1.896,8.087c0.002,0.113,0.017,0.228,0.02,0.341H36.32C36.279,24.671,36.243,24.337,36.232,23.985z"></path><path fill="#0883d9" d="M30.565,7.063C32.261,5.191,33.21,2.621,33.06,0c-2.346,0-5.066,1.372-6.788,3.394	c-1.348,1.672-2.795,4.293-2.271,6.913C26.422,10.607,29.043,9.085,30.565,7.063z"></path><path fill="#0370c8" d="M17.511,45c2.771,0,3.794-1.848,7.413-1.848c3.37,0,4.418,1.848,7.338,1.848	c3.07,0,5.092-2.795,6.913-5.567c2.295-3.218,3.07-6.288,3.169-6.414c-0.094,0-5.287-2.112-6.026-8.019H5.678	c0.157,5.311,2.228,10.79,4.671,14.309C12.27,42.055,14.441,45,17.511,45z"></path></svg>
+          <h4>Servicios</h4>
+          <p className="contenedor__parrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock</p>
+    </div>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="96" height="96"
+          viewBox="0 0 48 48"
+          style=" fill:#000000;"><path fill="#0883d9" d="M36.232,23.985c0-5.865,4.766-8.51,4.966-8.636c-2.596-3.993-6.79-4.467-8.362-4.468	c-3.643,0-6.863,2.022-8.585,2.022c-1.797,0-4.418-2.121-7.363-2.022c-3.843,0.075-7.363,2.346-9.334,5.691	c-1.397,2.396-1.947,5.217-1.896,8.087c0.002,0.113,0.017,0.228,0.02,0.341H36.32C36.279,24.671,36.243,24.337,36.232,23.985z"></path><path fill="#0883d9" d="M30.565,7.063C32.261,5.191,33.21,2.621,33.06,0c-2.346,0-5.066,1.372-6.788,3.394	c-1.348,1.672-2.795,4.293-2.271,6.913C26.422,10.607,29.043,9.085,30.565,7.063z"></path><path fill="#0370c8" d="M17.511,45c2.771,0,3.794-1.848,7.413-1.848c3.37,0,4.418,1.848,7.338,1.848	c3.07,0,5.092-2.795,6.913-5.567c2.295-3.218,3.07-6.288,3.169-6.414c-0.094,0-5.287-2.112-6.026-8.019H5.678	c0.157,5.311,2.228,10.79,4.671,14.309C12.27,42.055,14.441,45,17.511,45z"></path></svg>
+          <h4>Servicios</h4>
+          <p className="contenedor__parrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock</p>
+    </div>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="96" height="96"
+          viewBox="0 0 48 48"
+          style=" fill:#000000;"><path fill="#0883d9" d="M36.232,23.985c0-5.865,4.766-8.51,4.966-8.636c-2.596-3.993-6.79-4.467-8.362-4.468	c-3.643,0-6.863,2.022-8.585,2.022c-1.797,0-4.418-2.121-7.363-2.022c-3.843,0.075-7.363,2.346-9.334,5.691	c-1.397,2.396-1.947,5.217-1.896,8.087c0.002,0.113,0.017,0.228,0.02,0.341H36.32C36.279,24.671,36.243,24.337,36.232,23.985z"></path><path fill="#0883d9" d="M30.565,7.063C32.261,5.191,33.21,2.621,33.06,0c-2.346,0-5.066,1.372-6.788,3.394	c-1.348,1.672-2.795,4.293-2.271,6.913C26.422,10.607,29.043,9.085,30.565,7.063z"></path><path fill="#0370c8" d="M17.511,45c2.771,0,3.794-1.848,7.413-1.848c3.37,0,4.418,1.848,7.338,1.848	c3.07,0,5.092-2.795,6.913-5.567c2.295-3.218,3.07-6.288,3.169-6.414c-0.094,0-5.287-2.112-6.026-8.019H5.678	c0.157,5.311,2.228,10.79,4.671,14.309C12.27,42.055,14.441,45,17.511,45z"></path></svg>
+          <h4>Servicios</h4>
+          <p className="contenedor__parrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock</p>
+    </div>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+          width="96" height="96"
+          viewBox="0 0 48 48"
+          style=" fill:#000000;"><path fill="#0883d9" d="M36.232,23.985c0-5.865,4.766-8.51,4.966-8.636c-2.596-3.993-6.79-4.467-8.362-4.468	c-3.643,0-6.863,2.022-8.585,2.022c-1.797,0-4.418-2.121-7.363-2.022c-3.843,0.075-7.363,2.346-9.334,5.691	c-1.397,2.396-1.947,5.217-1.896,8.087c0.002,0.113,0.017,0.228,0.02,0.341H36.32C36.279,24.671,36.243,24.337,36.232,23.985z"></path><path fill="#0883d9" d="M30.565,7.063C32.261,5.191,33.21,2.621,33.06,0c-2.346,0-5.066,1.372-6.788,3.394	c-1.348,1.672-2.795,4.293-2.271,6.913C26.422,10.607,29.043,9.085,30.565,7.063z"></path><path fill="#0370c8" d="M17.511,45c2.771,0,3.794-1.848,7.413-1.848c3.37,0,4.418,1.848,7.338,1.848	c3.07,0,5.092-2.795,6.913-5.567c2.295-3.218,3.07-6.288,3.169-6.414c-0.094,0-5.287-2.112-6.026-8.019H5.678	c0.157,5.311,2.228,10.79,4.671,14.309C12.27,42.055,14.441,45,17.511,45z"></path></svg>
+          <h4>Servicios</h4>
+          <p className="contenedor__parrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock</p>
+    </div>
+  </section>
+  </header>
+          );
+        };
+
+export default Especiales;
---
+++ Login/src/Nosotros.js
@@ -0,0 +1,23 @@
+import './Casa.css';
+
+function Nosotros() {
+    return (
+
+      <section className="u-align-center u-clearfix u-custom-color-2 u-section-2" id="carousel_2b3e">
+        <div className="u-clearfix u-sheet u-sheet-1">
+          <h3 className="u-align-justify u-custom-font u-font-merriweather u-text u-text-1" data-lang-en="​Do you want to start a <span style=&quot;font-weight: 700;&quot;>business</span>, but do have experience or doubt? We invite you to cooperate. Sed ut <span style=&quot;font-weight: 700;&quot;>perspiciatis </span>unde omnis iste natus error."> Panadería La Única es una
+            microempresa colombiana, ubicada en la ciudad de Sincelejo - Sucre, dedicada a
+            ofrecer productos de Panadería, Pastelería y Cafetería, usando los mejores
+            sabores de la región caribe,que
+            para el año 2030 desea ser el líder del sector panificador, con alto
+            reconocimiento y crecimiento, cubriendo las necesidades de los Sincelejanos.<br />
+            <br />La Única&nbsp; ofrece un amplio catálogo en productos
+            alimenticios como buñuelos, borrachos, deditos horneados, torta porción, biscocho,
+            rollito arequipe, pizza, entre otros.&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          </h3>
+        </div>
+      </section>
+    );
+  };
+
+//export default Nosotros;
